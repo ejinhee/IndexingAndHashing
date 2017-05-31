8fbe7dd988ba2a6e2e2cdbf83eca98c87d829fc8
ejinhee <ejjinhee@gmail.com>
2017-05-31 17:55:27 +0900

    Add files via upload
    dynamic hash

--- HW2.cpp
+++ HW2.cpp
@@ -8,6 +8,8 @@
 #define TOKEN ","
 
 using namespace std;
+
+int num = 0;
 class Students {
 private:
 	char name[20];
@@ -54,7 +56,7 @@ public:
 	}
 	int insert(Students s) {
 		v.push_back(s);
-		if (v.capacity() == 128)
+		if (v.size() == 128)
 			return 1;
 		return 0;
 	}
@@ -63,89 +65,120 @@ class HashAddressTable {
 public:
 	Block** table;
 	int* prefix;
+	int count[1000]; //check pointer direct same block
 	int hat_size = 1;
 	int hat_cnt = 0;
 	HashAddressTable() {
+		for (int i = 0; i < 1000; i++)
+			count[i] = 0;
 	}
 	void resize() { // 블록중에 하나 꽉찰 때
 		int idx;
 		bool doubling;
 		for (int i = 0; i < hat_size; i++) {
-			if (table[i]->v.capacity() == 128) {
+			if (table[i]->v.size() == 128) {
 				idx = i;
 				break;
 			}
 		}
-		if (pow(2, idx) == (double)hat_size) { // ppt 밑에 2개중 하나 꽉찰 때 사이즈 2배
+		if (count[idx] ==0) { // ppt 밑에 2개중 하나 꽉찰 때 사이즈 2배
 			Block **temp = new Block*[2 * hat_size]; // table doubling
 			int *temp2 = new int[2 * hat_size]; //prefix doubling
 			for (int i = 0; i < hat_size; i++) {
-				temp[2 * i] = table[i];
-				temp[2 * i + 1] = table[i];
-				temp2[2 * i] = prefix[i];
-				temp2[2 * i + 1] = prefix[i];
-				temp2[2 * idx]++;
-				temp2[2 * idx+1]++;
-				Block newblock1,newblock2;
-				for (vector<Students>::iterator it = table[idx]->v.begin(); it != table[idx]->v.end(); it++) {
-					if ((int)pow(2, (prefix[idx] - 1)) & it->getID())
-						newblock2.insert(*it);
-					else
-						newblock1.insert(*it);
-				}
-				temp[2 * idx] = &newblock1;
-				temp[2 * idx + 1] = &newblock2;
-	
-				delete table;
-				table = temp;
-				delete prefix;
-				prefix = temp2;
+				temp2[i] = prefix[i];
+				temp2[i + hat_size] = prefix[i];
+				if (i == idx || i == idx + hat_size)
+					continue;
+				temp[i] = table[i];
+				temp[i + hat_size] = table[i];
+				
+				
 			}
+			temp2[idx]++;
+			temp2[idx + hat_size]++;
+			count[idx]--;
+			count[idx + hat_size]--;
+			Block* newblock1 = new Block;
+			Block* newblock2 = new Block;
+			for (vector<Students>::iterator it = table[idx]->v.begin(); it != table[idx]->v.end(); it++) {
+				if ((int)pow(2, (prefix[idx])) & it->getID())
+					newblock2->insert(*it);
+				else
+					newblock1->insert(*it);
+			}
+			temp[idx] = newblock1;
+			temp[idx + hat_size] = newblock2;
+			delete table;
+			table = temp;
+			delete prefix;
+			prefix = temp2;		
 			hat_size *= 2;
+			for (int i = 0; i < hat_size; i++)
+				count[i]++;
 			hat_cnt++;
 		}
 		else { //  ppt 위에 2개 꽉찰때
 			for (int i = 0; i < hat_size; i++) {
-				if (table[i]->v.capacity() == 128) {
+				if (table[i]->v.size() == 128) {
 					idx = i;
 					break;
 				}
 			}
-			int diff = hat_cnt - prefix[idx]; // ppt 위에가 2개가 아닐 경우를 위해
-			for (int i = 0; i < diff; i++) {
-				prefix[idx+i]++;
-			}
-			Block newblock1, newblock2;
+			Block* newblock1 = new Block;
+			Block* newblock2 = new Block;
 			for (vector<Students>::iterator it = table[idx]->v.begin(); it != table[idx]->v.end(); it++) {
-				if ((int)pow(2, (prefix[idx] - 1)) & it->getID())
-					newblock2.insert(*it);
+				if ((int)pow(2, (prefix[idx])) & it->getID())
+					newblock2->insert(*it);
 				else
-					newblock1.insert(*it);
+					newblock1->insert(*it);
 			}
+			Block* tempblockaddr;
+			tempblockaddr = table[idx];
 			delete table[idx];
-			for(int i=0; i< (int)pow(2,diff)/2;i++ )
-				table[idx+i] = &newblock1;
-			for (int i = (int)pow(2,diff)-1; i>= (int)pow(2, diff) / 2; i--)
-				table[idx + i] = &newblock2;	
+			bool chk = false;
+			for (int i = 0; i < hat_size; i++) {
+				if (table[i] == tempblockaddr) {
+					count[i]--;
+					if (!chk) {
+						table[i] = newblock1;
+						chk = true;
+						prefix[i]++;
+					}
+					else {
+						table[i] = newblock2;
+						chk = false;
+						prefix[i]++;
+					}
+				}
+			}
 		}
 	}
 };
 
 class DynamicHash {
 private:
-	Block b;
-	HashAddressTable hat;
+	Block* b;
+	HashAddressTable* hat;
 public:
 	DynamicHash(Block &b, HashAddressTable &hat) {
-		this->b = b;
-		this->hat = hat;
+		this->b = &b;
+		this->hat = &hat;
 
 		hat.table = new Block*[1];
 		hat.table[0] = &b;
 		hat.prefix = new int[1];
 		hat.prefix[0] = 0;
 	}
-
+	void insert(Students stu) {
+		int idx = stu.getID() % hat->hat_size;
+		if (hat->table[idx]->insert(stu))
+			hat->resize();
+	}
+	void print() {
+		cout << hat->hat_size << endl;
+		for (int i = 0; i < hat->hat_size; i++)
+			cout << hat->table[i]->v.size() << endl;
+	}
 };
 int main() {
 	ifstream fin("sampleData.csv");
@@ -164,7 +197,9 @@ int main() {
 	fin >> n;
 	fin.getline(temp, 50); // 개행
 	
+	Block b;
 	HashAddressTable hat;
+	DynamicHash dh(b, hat);
 	for (int i = 0; i < n; i++) {
 		fin.getline(temp, 50);
 		token = strtok_s(temp, TOKEN, &context);
@@ -181,31 +216,15 @@ int main() {
 		stu.setAdvisorID(advid);
 		
 		if(fout.write((char*)&stu, sizeof stu));
+		num++;
+		dh.insert(stu);
 
-
-		
-
-	
-		
 	}
 	
 	//Students stuarr[4000];
 	//ifstream inf("Students.DB", ios_base::in | ios_base::binary);
 	//int count = 0;
-
 	
-	//while (inf.read((char*)&stuarr, 128000)) 
-	int *a;
-	a = new int[3];
-	a[0] = 1;
-	a[1] = 2;
-	a[2] = 3;
-	cout << a[0] << endl;
-	int *t = new int[6];
-	t[0] = 2;
-	delete a;
-	a = t;
-	cout << sizeof(a);
 	
 	fout.close();
 	//stu.printInfo();
