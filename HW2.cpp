0c17fe0bce24612ee52f16f4acdefa2c118581a1
ejinhee <ejjinhee@gmail.com>
2017-05-31 23:20:53 +0900

    Add files via upload
    dynamic hash final

--- HW2.cpp
+++ HW2.cpp
@@ -64,17 +64,15 @@ public:
 class HashAddressTable {
 public:
 	Block** table;
-	int* prefix;
+	int* prefix; // ppt prefix ( 몇번째 비트로 나누었는지)
 	int count[1000]; //check pointer direct same block
-	int hat_size = 1;
-	int hat_cnt = 0;
+	int hat_size = 1; // hash address table 크기 ppt왼쪽
 	HashAddressTable() {
 		for (int i = 0; i < 1000; i++)
-			count[i] = 0;
+			count[i] = 0; // count 값이 0이 아니면 같은 블록을 가르키는 hash address table의 원소가 1개가 아닌 2개 이상. 블록이 꽉차도 hash address table의 크기는 늘어나지 않음.
 	}
 	void resize() { // 블록중에 하나 꽉찰 때
 		int idx;
-		bool doubling;
 		for (int i = 0; i < hat_size; i++) {
 			if (table[i]->v.size() == 128) {
 				idx = i;
@@ -83,7 +81,7 @@ public:
 		}
 		if (count[idx] ==0) { // ppt 밑에 2개중 하나 꽉찰 때 사이즈 2배
 			Block **temp = new Block*[2 * hat_size]; // table doubling
-			int *temp2 = new int[2 * hat_size]; //prefix doubling
+			int *temp2 = new int[2 * hat_size]; //prefix table doubling
 			for (int i = 0; i < hat_size; i++) {
 				temp2[i] = prefix[i];
 				temp2[i + hat_size] = prefix[i];
@@ -101,7 +99,7 @@ public:
 			Block* newblock1 = new Block;
 			Block* newblock2 = new Block;
 			for (vector<Students>::iterator it = table[idx]->v.begin(); it != table[idx]->v.end(); it++) {
-				if ((int)pow(2, (prefix[idx])) & it->getID())
+				if ((int)pow(2, (prefix[idx])) & it->getID()) // 넘쳐서 분배
 					newblock2->insert(*it);
 				else
 					newblock1->insert(*it);
@@ -115,9 +113,8 @@ public:
 			hat_size *= 2;
 			for (int i = 0; i < hat_size; i++)
 				count[i]++;
-			hat_cnt++;
 		}
-		else { //  ppt 위에 2개 꽉찰때
+		else { //  ppt 위에 2개 꽉찰때 count 값이 0이 아니기 때문에 hat 사이즈는 늘어나지 않음.
 			for (int i = 0; i < hat_size; i++) {
 				if (table[i]->v.size() == 128) {
 					idx = i;
@@ -137,7 +134,7 @@ public:
 			delete table[idx];
 			bool chk = false;
 			for (int i = 0; i < hat_size; i++) {
-				if (table[i] == tempblockaddr) {
+				if (table[i] == tempblockaddr) { 
 					count[i]--;
 					if (!chk) {
 						table[i] = newblock1;
@@ -220,11 +217,6 @@ int main() {
 		dh.insert(stu);
 
 	}
-	
-	//Students stuarr[4000];
-	//ifstream inf("Students.DB", ios_base::in | ios_base::binary);
-	//int count = 0;
-	
 	
 	fout.close();
 	//stu.printInfo();
